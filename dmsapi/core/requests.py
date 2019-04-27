870e2bdeac0b81c5e725a3fd82bf04cc3067da2a
moreal <dev.moreal@gmail.com>
2019-04-27 22:56:29 +0900

    Implement proxy module for ApiCallAuth

---
+++ dmsapi/core/requests.py
@@ -0,0 +1,15 @@
+from typing import AnyStr, Callable, Any
+
+import requests
+
+
+from dmsapi.core.auth import ApiCallAuth
+
+api_call_auth = ApiCallAuth()
+
+
+def __getattr__(name: AnyStr) -> Callable[[AnyStr, Any], requests.Response]:
+    def request(url, **kwargs) -> requests.Response:
+        return getattr(requests, name)(url, auth=api_call_auth, **kwargs)
+
+    return request
