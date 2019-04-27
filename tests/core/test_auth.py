0faf346a8b13a697d8bb7ad26c097c9b5df553e1
moreal <dev.moreal@gmail.com>
2019-04-27 22:53:42 +0900

    Add testcode for dmsapi.core.auth

---
+++ tests/core/test_auth.py
@@ -0,0 +1,26 @@
+from dmsapi import config
+from dmsapi.core import requests
+from requests import Request
+from dmsapi.utils.auth import make_user_data
+
+ROOT_PATH = config.entrypoints['ROOT']
+
+
+def check_user_data_callback(request: Request, context):
+    user_agent = request.headers.get('User-Agent', None)
+    x_date = request.headers.get('X-Date', None)
+    user_data = request.headers.get('User-Data', None)
+
+    if user_agent is None or x_date is None or user_data is None or user_data != make_user_data(user_agent, x_date):
+        context.status_code = 418
+        return "I'm a teapot"
+    else:
+        context.status_code = 404
+        return 'Not Found'
+
+
+def test_request_with_auth(requests_mock):
+    requests_mock.get(ROOT_PATH, text=check_user_data_callback)
+    resp = requests.get(ROOT_PATH)
+
+    assert 418 != resp.status_code
