baa0b85f7819fb3f88302caa525865ad7284f584
moreal <dev.moreal@gmail.com>
2019-05-12 23:58:16 +0900

    Implement required_auth

--- dmsapi/api/base.py
+++ dmsapi/api/base.py
@@ -1,4 +1,16 @@
+from typing import Callable, Any
+
 from dmsapi.core.session import DMSAccountSession
+
+
+def require_auth(func: Callable[..., Any], *args):
+    def wrap():
+        self: BaseAPI = args[0]
+        session = self.session
+        if session.is_authenticated:
+            func(*args)
+
+    return wrap
 
 
 class BaseAPI:
@@ -14,3 +26,7 @@ class BaseAPI:
 
     def get(self):
         pass
+
+    @property
+    def session(self):
+        return self._session
