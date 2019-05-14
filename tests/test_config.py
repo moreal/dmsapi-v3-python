08e176c9325fbea51ca455e822360345d59ac11e
moreal <dev.moreal@gmail.com>
2019-05-14 09:02:20 +0900

    Update url in test_config

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,4 +2,4 @@ from dmsapi import config
 
 
 def test_get_entrypoints():
-    assert config.entrypoints['ROOT'] == 'https://api.dms.istruly.sexy/'
+    assert config.entrypoints['ROOT'] == 'https://api.dms.istruly.sexy'
