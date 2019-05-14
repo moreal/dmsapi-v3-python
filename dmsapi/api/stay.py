016e804df9b6a43f323d106cfd07c6fc69e2d7a6
moreal <dev.moreal@gmail.com>
2019-05-14 11:28:11 +0900

    Implement StayAPI

--- dmsapi/api/stay.py
+++ dmsapi/api/stay.py
@@ -1,5 +1,28 @@
-from dmsapi.api.base import BaseAPI
+from enum import IntEnum
+
+from dmsapi import config
+from dmsapi.api.base import BaseAPI, require_auth
+
+
+class StayType(IntEnum):
+    FRIDAY_HOMECOMING = 1
+    SATURDAY_HOMECOMING = 2
+    SATURDAY_RETURN_DORMITORY = 3
+    STAY = 4
 
 
 class Stay(BaseAPI):
-    pass
+    @require_auth
+    def get(self) -> StayType:
+        return self.session.get(
+            config.entrypoints['STAY']
+        ).json()['value']
+
+    @require_auth
+    def apply(self, _value) -> bool:
+        return self.session.post(
+            f"{config.entrypoints['STAY']}",
+            json={
+                'value': _value
+            }
+        ).status_code == 200
