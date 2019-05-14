e5aabfd25db2113d9de443bcc4c6ba01566ad617
moreal <dev.moreal@gmail.com>
2019-05-14 11:10:05 +0900

    Implement MealAPI

--- dmsapi/api/meal.py
+++ dmsapi/api/meal.py
@@ -1,5 +1,11 @@
+from typing import AnyStr
+
+from dmsapi import config
 from dmsapi.api.base import BaseAPI
 
 
 class Meal(BaseAPI):
-    pass
+    def get(self, _date: AnyStr):
+        return self.session.get(
+            f"{config.entrypoints['MEAL']}/{_date}"
+        ).json()
